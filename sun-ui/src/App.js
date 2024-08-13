146894828738dccbb2aa16525b3fd4b6400ef106
leesehyun <sehyun5795@gmail.com>
2024-08-13 15:19:05 +0900

    wbs[0813]feat: app.js 로그인/로그아웃 페이지 추가 - SONIC

--- sun-ui/src/App.js
+++ sun-ui/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css';
-import Login from './commodule/LoginComponent';
-import Home from './commodule/Home';
+import Login from './login/LoginComponent';
+import LogoutComponent from './login/LogoutComponent';
 function App() {
   return (
     <div>
       <div>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/logout" element={<LogoutComponent />} />
         </Routes> 
       </div>
     </div>
---
+++ sun-ui/src/login/LoginComponent.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const LoginComponent = () => {
+    const navigate = useNavigate();
+
+    const storedUserData = window.sessionStorage.getItem("user");
+    const initialUser = storedUserData ? JSON.parse(storedUserData) : { empcode: '', emppw: '' };
+
+    const [user, setUser] = useState(initialUser);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const formData = new FormData();
+          formData.append('empcode', user.empcode);
+          formData.append('emppw', user.emppw);
+    
+          console.log('전송된 empcode:', user.empcode); // 확인
+            console.log('전송된 emppw:', user.emppw); // 확인
+          const response = await axios({
+            url: 'http://localhost:8787/loginProc',
+            method: 'POST',
+            data: formData,
+            withCredentials: true,
+          });
+          console.log('서버 응답:', response.data); // 응답 확인
+          if (response.status === 200) {
+            alert('로그인 성공!');
+            console.log('유저 이메일: ' + response.data.empcode);
+            console.log('권한: ' + response.data.emppw);
+            
+             // 세션 스토리지에 사용자 데이터 저장 (JSON 형식)
+            const userData = {
+                empcode: response.data.empcode,
+                authorities: response.data.authorities,
+                
+            };
+    
+            window.sessionStorage.setItem("user", JSON.stringify(userData));
+    
+            navigate('/logout', { state: { userData: response.data } });
+          }
+        } catch (error) {
+          alert('로그인 실패');
+        }
+      };
+
+    return (
+        <div>
+            <h3>로그인</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="empcode" placeholder="ID" value={user.empcode} onChange={handleChange} />
+                <input type="password" name="emppw" placeholder="PW" value={user.emppw} onChange={handleChange} />
+                <button type="submit">로그인</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginComponent;
