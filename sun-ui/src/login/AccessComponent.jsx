592283fe8c6dbf4ba7d1460703caddea4f9f6018
bluewt129 <bluewt129@gmail.com>
2024-08-14 09:34:53 +0900

    WBS[240814]feat: AccessComponent 기능 수정, Header 수정 - blue

---
+++ sun-ui/src/login/AccessComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContentComponent from '../maincontent/MainContentComponent';
+import HeaderTopComponent from '../layout/HeaderTopComponent';
+
+const AccessComponent = () => {
+    return (
+        <div>
+            <HeaderTopComponent/>
+            <MainContentComponent/>
+        </div>
+    );
+};
+
+export default AccessComponent;
