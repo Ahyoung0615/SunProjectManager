c4d4e1db07253331c74819c3222d9deb4efecd9b
leesehyun <sehyun5795@gmail.com>
2024-08-30 16:39:00 +0900

    WBS[2024-08-30]-[feat]: 전체적인수정 (1차) SONIC

--- sun-ui/src/member/MemberDetailComponent.jsx
+++ sun-ui/src/member/MemberDetailComponent.jsx
@@ -11,9 +11,9 @@ const MemberDetailComponent = () => {
 
     const handleShow = () => setShowModal(true);
     const handleClose = async () => {
-      setShowModal(false);
-      await fetchEmployeeData(); // 비동기 데이터 새로고침
-  };
+        setShowModal(false);
+        await fetchEmployeeData(); // 비동기 데이터 새로고침
+    };
 
     // 사원 정보를 가져오는 useEffect
     useEffect(() => {
@@ -25,9 +25,10 @@ const MemberDetailComponent = () => {
                 }
                 const data = await response.json();
                 setEmployee(data);
-                setEmpImg(`http://localhost:8787/memberImage/${data.empImg}`); // 이미지 URL 업데이트
+                setEmpImg(data.empImg ? `http://localhost:8787/memberImage/${data.empImg}` : '/img/noimages.png'); // 이미지 URL 업데이트
             } catch (error) {
                 console.error('Error fetching employee data:', error);
+                setEmpImg('/img/noimages.png'); // 에러 발생 시 기본 이미지
             }
         };
 
@@ -72,12 +73,14 @@ const MemberDetailComponent = () => {
             }
             const data = await response.json();
             setEmployee(data);
-            setEmpImg(`http://localhost:8787/memberImage/${data.empImg}`); // 이미지 URL 업데이트
+            setEmpImg(data.empImg ? `http://localhost:8787/memberImage/${data.empImg}` : '/img/noimages.png'); // 이미지 URL 업데이트
         } catch (error) {
             console.error('Error fetching employee data:', error);
+            setEmpImg('/img/noimages.png'); // 에러 발생 시 기본 이미지
         }
     };
-      const getJobTitle = (jobCode) => {
+
+    const getJobTitle = (jobCode) => {
         switch (jobCode) {
             case 1:
                 return '대표';
@@ -100,27 +103,32 @@ const MemberDetailComponent = () => {
         }
     };
 
-    const getGender = (gender) =>{
-      switch(gender){
-        case 'F': 
-            return '여성';
-        case 'M': 
-            return '남성';
-      }
-    }
+    const getGender = (gender) => {
+        switch (gender) {
+            case 'F':
+                return '여성';
+            case 'M':
+                return '남성';
+            default:
+                return '정보 없음';
+        }
+    };
 
-    const getStatus = (empStatus) =>{
-      switch(empStatus){
-        case 'Y':
-          return '재직';
-        case 'N':
-          return '퇴사';
-        case 'V':
-          return '휴가';
-      }
-    }
+    const getStatus = (empStatus) => {
+        switch (empStatus) {
+            case 'Y':
+                return '재직';
+            case 'N':
+                return '퇴사';
+            case 'V':
+                return '휴가';
+            default:
+                return '정보 없음';
+        }
+    };
+
     const getDeptTitle = (deptCode) => {
-        switch (deptCode){
+        switch (deptCode) {
             case 1:
                 return '경영총괄';
             case 11:
@@ -137,109 +145,109 @@ const MemberDetailComponent = () => {
                 return '자재관리';
             case 71:
                 return '생산제조';
+            default:
+                return '정보 없음';
         }
     };
 
     const handleFileChange = (e) => {
-      setFile(e.target.files[0]);
-  };
+        setFile(e.target.files[0]);
+    };
 
-  const handlePasswordReset = async () => {
-      try {
-          const response = await fetch(`http://localhost:8787/resetPassword/${empCode}`, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-          });
+    const handlePasswordReset = async () => {
+        try {
+            const response = await fetch(`http://localhost:8787/resetPassword/${empCode}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
 
-          const message = await response.text();
-          alert(message);
-      } catch (error) {
-          alert('비밀번호 초기화 중 오류가 발생했습니다.');
-      }
-  };
+            const message = await response.text();
+            alert(message);
+        } catch (error) {
+            alert('비밀번호 초기화 중 오류가 발생했습니다.');
+        }
+    };
 
-  return (
-      <div className="container" style={{ marginTop: 30 }}>
-          <h1>사원 상세</h1>
-          <div style={{ display: "flex", alignItems: "flex-start", marginBottom: 20 }}>
-              <div style={{ marginRight: 20, textAlign: "center" }}>
-                  <div style={{ width: "300px", height: "300px", backgroundColor: "#ccc", marginBottom: 10 }}>
-                      {empImg ? (
-                          <img src={empImg} alt="사원 이미지" style={{ width: "100%", height: "100%" }} />
-                      ) : (
-                          <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", color: "#888" }}>
-                              이미지 없음
-                          </div>
-                      )}
-                  </div>
-                  <label>파일 업로드:</label>
-                  <input type="file" onChange={handleFileChange} />
-                  <br />
-                  <button className="btn btn-primary" onClick={handleUpload}>업로드</button>
-              </div>
-              <div style={{ flex: 2 }}>
-                  <table className="table table-bordered">
-                      <tbody>
-                          <tr>
-                              <td>사번</td>
-                              <td>{employee.empCode}</td>
-                          </tr>
-                          <tr>
-                              <td>이름</td>
-                              <td>{employee.empName}</td>
-                          </tr>
-                          <tr>
-                              <td>직급</td>
-                              <td>{getJobTitle(employee.jobCode)}</td>
-                          </tr>
-                          <tr>
-                              <td>부서</td>
-                              <td>{getDeptTitle(employee.deptCode)}</td>
-                          </tr>
-                          <tr>
-                              <td>성별</td>
-                              <td>{getGender(employee.gender)}</td>
-                          </tr>
-                          <tr>
-                              <td>전화번호</td>
-                              <td>{employee.empTel}</td>
-                          </tr>
-                          <tr>
-                              <td>이메일</td>
-                              <td>{employee.empEmail}</td>
-                          </tr>
-                          <tr>
-                              <td>주소</td>
-                              <td>{employee.empAddress}</td>
-                          </tr>
-                          <tr>
-                              <td>입사일</td>
-                              <td>{employee.joindate}</td>
-                          </tr>
-                          <tr>
-                              <td>근무현황</td>
-                              <td>{getStatus(employee.empStatus)}</td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </div>
-          </div>
-          <button className="btn btn-primary" onClick={handleShow} style={{ position: 'absolute', right: '350px' }}>
-              사원 정보 수정
-          </button>
-          <MemberUpdateModal show={showModal} handleClose={handleClose} />
+    return (
+        <div className="container" style={{ marginTop: 30 }}>
+            <h1>사원 상세</h1>
+            <div style={{ display: "flex", alignItems: "flex-start", marginBottom: 20 }}>
+                <div style={{ marginRight: 20, textAlign: "center" }}>
+                    <div style={{ width: "300px", height: "300px", backgroundColor: "#ccc", marginBottom: 10 }}>
+                        <img
+                            src={empImg}
+                            alt="사원 이미지"
+                            style={{ width: "100%", height: "100%" }}
+                        />
+                    </div>
+                    <label>파일 업로드:</label>
+                    <input type="file" onChange={handleFileChange} />
+                    <br />
+                    <button className="btn btn-primary" onClick={handleUpload}>업로드</button>
+                </div>
+                <div style={{ flex: 2 }}>
+                    <table className="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <td>사번</td>
+                                <td>{employee.empCode}</td>
+                            </tr>
+                            <tr>
+                                <td>이름</td>
+                                <td>{employee.empName}</td>
+                            </tr>
+                            <tr>
+                                <td>직급</td>
+                                <td>{getJobTitle(employee.jobCode)}</td>
+                            </tr>
+                            <tr>
+                                <td>부서</td>
+                                <td>{getDeptTitle(employee.deptCode)}</td>
+                            </tr>
+                            <tr>
+                                <td>성별</td>
+                                <td>{getGender(employee.gender)}</td>
+                            </tr>
+                            <tr>
+                                <td>전화번호</td>
+                                <td>{employee.empTel}</td>
+                            </tr>
+                            <tr>
+                                <td>이메일</td>
+                                <td>{employee.empEmail}</td>
+                            </tr>
+                            <tr>
+                                <td>주소</td>
+                                <td>{employee.empAddress}</td>
+                            </tr>
+                            <tr>
+                                <td>입사일</td>
+                                <td>{employee.joindate}</td>
+                            </tr>
+                            <tr>
+                                <td>근무현황</td>
+                                <td>{getStatus(employee.empStatus)}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <button className="btn btn-primary" onClick={handleShow} style={{ position: 'absolute', right: '350px' }}>
+                사원 정보 수정
+            </button>
+            <MemberUpdateModal show={showModal} handleClose={handleClose} />
 
-          <button className="btn btn-primary" onClick={handlePasswordReset} style={{ position: 'absolute', right: '190px' }}>
-              비밀번호 초기화
-          </button>
-      </div>
-  );
+            <button className="btn btn-primary" onClick={handlePasswordReset} style={{ position: 'absolute', right: '190px' }}>
+                비밀번호 초기화
+            </button>
+        </div>
+    );
 };
 
 export default MemberDetailComponent;
