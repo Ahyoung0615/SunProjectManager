c73316a099fcac2ad80f23475c3ce4bd34dfe425
bluewt129 <bluewt129@gmail.com>
2024-09-09 09:10:31 +0900

    WBS[2024-09-09]feat: Swagger 적용 - blue

--- SunApi/src/main/java/com/brs/sun/controller/DayOffController.java
+++ SunApi/src/main/java/com/brs/sun/controller/DayOffController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.brs.sun.jpa.entity.DayOffEntity;
 import com.brs.sun.jpa.service.DayOffJpaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name="DayOff Controller", description = "연차관리 조회 컨트롤러")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +26,12 @@ public class DayOffController {
 	
 	private final DayOffJpaService service;
 	
+	@Operation(summary = "연차 조회", description = "사원 코드를 통한 개인 연차 조회")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "개인 연차 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = DayOffEntity.class))),
+		@ApiResponse(responseCode = "400", description = "잘못된 요청"),
+		@ApiResponse(responseCode = "500", description = "서버 에러")
+	})
 	@GetMapping("/{empCode}")
 	public DayOffEntity getMyDayOff(@PathVariable Long empCode) {
 	    log.info("연차조회 getMyDayOff empCode : {}", empCode);
--- SunApi/src/main/java/com/brs/sun/controller/MinutesController.java
+++ SunApi/src/main/java/com/brs/sun/controller/MinutesController.java
@@ -16,9 +16,16 @@ import com.brs.sun.jpa.service.MinutesJpaService;
 import com.brs.sun.model.service.JsTreeService;
 import com.brs.sun.vo.EmployeeVo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "회의록 Controller", description = "회의록 MyBatis CRUD Controller")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +35,12 @@ public class MinutesController {
 	private final MinutesJpaService service;
 	private final JsTreeService jsService;
 	
+	 @Operation(summary = "회의록 리스트 조회", description = "특정 사번(empCode)을 기반으로 회의록 목록을 페이징 처리하여 조회합니다.")
+	    @ApiResponses({
+	        @ApiResponse(responseCode = "200", description = "성공적으로 회의록 목록을 가져왔습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MinutesEntity.class))),
+	        @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+	        @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+	    })
 	@GetMapping("/minutesList")
 	public Page<MinutesEntity> getMyMinutesList(@RequestParam Integer empCode,
 	                                           @RequestParam(defaultValue = "0") int page, 
@@ -40,24 +53,42 @@ public class MinutesController {
 	    return service.getMyMinutesList(empCode, page, size);
 	}
 	
+	 @Operation(summary = "회의록 상세 조회", description = "회의록 코드를 기반으로 특정 회의록의 상세 정보를 조회합니다.")
+	    @ApiResponses({
+	        @ApiResponse(responseCode = "200", description = "성공적으로 회의록 정보를 가져왔습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MinutesEntity.class))),
+	        @ApiResponse(responseCode = "404", description = "해당 회의록 정보를 찾을 수 없습니다."),
+	        @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+	    })
 	@GetMapping("/minutesDetail/{minutesCode}")
 	public MinutesEntity findByMinutesCode(@PathVariable Long minutesCode) {
 		log.info("findByMinutesCode 요청 회의록번호 : {}", minutesCode);
 		return service.findByMinutesCode(minutesCode);
 	}
 	
+	 @Operation(summary = "회의록 저장", description = "새로운 회의록을 추가합니다.")
+	    @ApiResponses({
+	        @ApiResponse(responseCode = "200", description = "회의록이 성공적으로 저장되었습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MinutesEntity.class))),
+	        @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+	        @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+	    })
 	@PostMapping("/insertMinutes")
 	public MinutesEntity saveMinutes(@RequestBody MinutesEntity minutes) {
 		log.info("저장할 MinutesEntity 정보 : {}", minutes);
 		return service.saveMinutes(minutes);
 	}
 	
+	 @Operation(summary = "회의 참석자 정보 조회", description = "선택된 참석자 리스트를 기반으로 회의 참석자 정보를 조회합니다.")
+	    @ApiResponses({
+	        @ApiResponse(responseCode = "200", description = "참석자 정보를 성공적으로 가져왔습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeVo.class))),
+	        @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+	        @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+	    })
 	@PostMapping("/mintesAttendense")
 	public List<EmployeeVo> getApprovers(@RequestBody List<String> choiceList) {
 	    log.info("choiceArr: {}", choiceList);
 	    
-	    List<EmployeeVo> approverList = jsService.getApprovers(choiceList);
-	    
-	    return approverList;
+		List<EmployeeVo> approverList = jsService.getApprovers(choiceList);
+
+		return approverList;
 	}
 }
